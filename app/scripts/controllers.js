04aed107a5b7d77dd173b0a9fd9e066ab8b62958
eugenebelov <eugene.beloff@gmail.com>
2015-04-25 06:24:57 +0300

    add google maps support

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -13,7 +13,6 @@ sessionsControllers.controller('SessionsController', function ($scope, $routePar
             $scope.sessionsList = value.run_sessions;
 
             $scope.sessionsList.forEach(function(item) {
-
                 item.start_time = $filter('date')(item.start_time, 'dd.MM.yyyy, HH.mm');
                 item.end_time = $filter('date')(item.end_time, 'dd.MM.yyyy, HH.mm');
                 item.hasTrace = (item.encoded_trace) ? true : false
@@ -37,14 +36,40 @@ sessionsControllers.controller('SessionsController', function ($scope, $routePar
 
 });
 
-sessionsControllers.controller('SessionsDetailsController', function($scope, $routeParams, GetSessionDetails) {
+sessionsControllers.controller('SessionsDetailsController', function($scope, $routeParams, $filter, GetSessionDetails) {
     $scope.sessionsDetails = {};
     $scope.currentPage = $routeParams.pageNum;
+
+    var mapOptions = {
+        zoom: 14,
+        center: new google.maps.LatLng(40.0000, -98.0000),
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        libraries: ['google.maps.geometry']
+    };
 
     GetSessionDetails.get({sessionId: $routeParams.id}).$promise.then(
         function( value ) {
             $scope.sessionsDetails = value.run_session;
+            $scope.sessionsDetails.start_time = $filter('date')($scope.sessionsDetails.start_time, 'dd.MM.yyyy, HH.mm');
+            $scope.sessionsDetails.end_time = $filter('date')($scope.sessionsDetails.end_time, 'dd.MM.yyyy, HH.mm');
+
+            $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+            var path = google.maps.geometry.encoding.decodePath($scope.sessionsDetails.encoded_trace);
+
+            $scope.map.setCenter(path[0]);
+
+            var poly = new google.maps.Polyline({
+                path: path,
+                geodesic: true,
+                strokeColor: '#FF0000',
+                strokeOpacity: 1.0,
+                strokeWeight: 2
+            });
+
+            poly.setMap($scope.map)
         },
         function( error ){/*error*/}
     );
+
+
 });
